b5b99d1ac0fcc291c2f16d713bca5cbddc8dd04d
uphayled <1364014@johnabbottcollege.net>
2017-01-27 15:22:39 -0500

    class 4 middle
    ..

---
+++ class 4.ps1
@@ -0,0 +1,53 @@
+﻿Clear
+cls
+
+ipconfig
+
+cls
+
+Get-Service
+
+cls
+
+get-Service * |where {$_.Status -eq "Stopped"}
+
+
+cls
+      
+get-Service * |Sort-Object -property ServiceType | format-Table name, ServiceType, status, CanStop, -auto
+
+cls
+
+#This script is a bit longer
+$strComputer = "."
+$colItems = get-wmiobject -class "Win32_NetworkAdapterConfiguration" `
+-computername $strComputer | Where{$_.IpEnabled -Match "True"}
+foreach ($objItem in $colItems) {
+   write-host "MAC Address : " $objItem.MACAddress
+   write-host "IPAddress : " $objItem.IPAddress
+   write-host "IPAddress : " $objItem.IPEnabled 
+   write-host ""
+}
+Write-Host "---------------------------------------------"
+Write-Host ""
+#######This script is a bit longer
+$strComputer = "."
+$colItems = get-wmiobject -class "Win32_NetworkAdapterConfiguration" `
+-computername $strComputer | Where{$_.IpEnabled -Match "True"}
+foreach ($objItem in $colItems) {
+   write-host "MAC Address : " $objItem.MACAddress
+   write-host "IPAddress : " $objItem.IPAddress
+   write-host "IPAddress : " $objItem.IPEnabled 
+   write-host "DNS Servers : " $objItem.DNSServerSearchOrder
+   write-host "Subnet : " $objItem.IPSubnet
+   write-host ""
+}
+
+
+cls
+# PowerShell script to list processes and group by company
+get-Process | sort company | format-Table ProcessName -groupby company
+
+
+
+
---
+++ process1.ps1
@@ -0,0 +1,3 @@
+﻿cls
+# PowerShell script to list processes and group by company
+get-Process | sort company | format-Table ProcessName -groupby company
