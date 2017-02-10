51975eb7832fa6d877fef3c6ad04acf2c3ee4645
uphayled <uphayled@gmail.com>
2017-02-10 15:10:53 -0500

    sdf
    sdf

--- AS2/script.ps1
+++ AS2/script.ps1
@@ -1,9 +1,9 @@
 Param(
-    [int]$id = 1
+    [int]$id = 2
 )
 switch ($id) 
 { 
-    1 {Uninstall-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
-    2 {Install-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
+    "1" {Uninstall-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
+    "2" {Install-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
    
 }
