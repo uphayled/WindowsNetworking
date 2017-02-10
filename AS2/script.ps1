f96499ba393ba2a5e27875d60b6479889e37f41a
uphayled <1364014@johnabbottcollege.net>
2017-02-10 15:01:24 -0500

    qwe
    qwe

--- AS2/script.ps1
+++ AS2/script.ps1
@@ -1,9 +1,9 @@
+Param(
+    [int]$id = 1
+)
 switch ($id) 
-        { 
-            "1" {Uninstall-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
-            "2" {Install-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
-            "3" { 
-                    Import-Module Dism 
-                    Enable-WindowsOptionalFeature -online -Featurename ServerCore-FullServer,Server-Gui-Shell,Server-Gui-Mgmt 
-                } 
-        }
+{ 
+    1 {Uninstall-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
+    2 {Install-WindowsFeature Server-Gui-Mgmt-Infra,Server-Gui-Shell} 
+   
+}
