2168f75d9dcd2a44574fa3bd7f780b0ec7eea7b9
uphayled <uphayled@gmail.com>
2017-02-10 15:05:46 -0500

    jkl
    jkl

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-﻿ $CU = net user Jim /addIf ($u =$false) {$CU}
+﻿ Get-process | Select-Object @{Label = "Name" ; Expression = {$_.Name }} ,@{Label = "Physical"; Expression = {$_.WS }},@{Label = "Virtual"; Expression = {$_.VM}},@{Label = "TotalMem"; Expression = {$_.WS + $_.VM}} | Sort-Object TotalMeme  -descending | ConvertTo-HTML | Out-File $srt\runningprocesses.htm
