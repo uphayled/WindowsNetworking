b05447783481137f805d01b371e49f2a2b2ba325
uphayled <1364014@johnabbottcollege.net>
2017-02-24 15:53:08 -0500

    sdf
    sdf

--- Assignment 4/AS3.ps1
+++ Assignment 4/AS3.ps1
@@ -5,46 +5,69 @@ $srt = $PSScriptRoot
 
 measure-command { 
 
-
+<#check and create "disks"
 $stu = "\stripe"
 foreach ($a in (1..3)){
     while (!(Test-Path -Path $srt$stu$a )){
         out-file -filepath $srt$stu$a
     }
     write-host "$stu$a file exists"
-}
- 
-$chunksize=1024
+}#>
+
+#READ IN
 $arr = New-Object System.Collections.ArrayList
-$arr = [io.file]::ReadAllBytes("$srt\MacBeth.txt")
-[io.file]::WriteAllBytes('c:\scripts\test2.jpg',$file)
+$arr = [io.file]::ReadAllBytes("$srt\macbeth.txt")
+
+#[io.file]::WriteAllBytes("$srt$stu",$arr)
 
 #foreach ($c in @(Get-Content $srt\"MacBeth.txt" -Encoding byte) ){
 #    $arr.add($c)
 #}
+
+
+
+
+#READ OUT
 $id=0
 foreach ($c in $arr){
     $a = (($i ++ )%3) +1
-    out-file -FilePath $srt$stu$a -InputObject $c
+    $j = [convert]::ToString([int32]$c,2)
+    out-file -FilePath $srt$stu$a -InputObject $j
 }
 
     
-    $r = ((get-random)%3) +1
-    write-host "deleting disk $r"
+$r = ((get-random)%3) +1
+write-host "deleting disk $r"
 
-    write-host "" | out-file -FilePath $srt$stu$r
-
-
+write-host "" | out-file -FilePath $srt$stu$r
 
 
 
+foreach ($i in (1..3)){
+    write-host  (get-content $srt$stu$i)
+}
 
-    if(!(Test-Path -Path $srt$stu"1" )){
-        get-content $srt$stu
+<# $dead=-1;
+$d1 
+$d2
+if(!(Test-Path -Path $srt$stu"1" )){
+    $d1 = get-content $srt$stu"2"
+    $d2 = get-content $srt$stu"3"
+    $dead = 1        
+}
+else{
+    if(!(Test-Path -Path $srt$stu"2")){
+        $d1 = get-content $srt$stu"1"
+        $d2 = get-content $srt$stu"3"
+        $dead = 2
+    }        
+    else{
+        $d1 = get-content $srt$stu"1"
+        $d2 = get-content $srt$stu"2"
+        $dead = 3
     }
-    else
-        if(!(Test-Path -Path $srt$stu$a)
 
+}#>
 
-    
+   
 }
