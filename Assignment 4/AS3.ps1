81fab586ce1b2eda647ab60272057f387b880d56
uphayled <1364014@johnabbottcollege.net>
2017-03-02 16:43:43 -0500

    qweq
    qwe

--- Assignment 4/AS3.ps1
+++ Assignment 4/AS3.ps1
@@ -3,7 +3,7 @@
 #root of where script is being run
 $srt = $PSScriptRoot
 
-measure-command { 
+$DISKAMOUNT = 3 
 
 <#check and create "disks"
 $stu = "\stripe"
@@ -14,19 +14,44 @@ foreach ($a in (1..3)){
     write-host "$stu$a file exists"
 }#>
 
-#READ IN
-$arr = New-Object System.Collections.ArrayList
-$arr = [io.file]::ReadAllBytes("$srt\macbeth.txt")
+#$arr# = [io.file]::ReadAllBytes("$srt\macbeth.txt")
 
 #[io.file]::WriteAllBytes("$srt$stu",$arr)
 
 #foreach ($c in @(Get-Content $srt\"MacBeth.txt" -Encoding byte) ){
 #    $arr.add($c)
 #}
+$id=0
+$line=3
+$read = New-Object System.IO.StreamReader("$srt\macbeth.txt")
+$enc = [system.Text.Encoding]::ASCII
+measure-command { 
+try {
+    while($read.Peek() -ge 0){
+        $f = [String[]] 
+        $b =  $enc.GetBytes($read.ReadLine()) 
+        foreach ($a in $b){
+             $j =  [convert]::ToString([int32]$a,2)
+             
+             foreach($h in $j ){
+                 $f[$id++ % $DISKAMOUNT] = $h 
+                 
+             }
+        }
+        #write-host $f
+        
+        
+        
+    }
 
+        
+}
+finally {
+    $reader.Close()
+}
 
-
-
+$stu = "\stripe"
+<#
 #READ OUT
 $id=0
 foreach ($c in $arr){
@@ -34,16 +59,16 @@ foreach ($c in $arr){
     $j = [convert]::ToString([int32]$c,2)
     out-file -FilePath $srt$stu$a -InputObject $j
 }
-
+#>
     
-$r = ((get-random)%3) +1
+$r = ((get-random)%$DISKAMOUNT) +1
 write-host "deleting disk $r"
 
 write-host "" | out-file -FilePath $srt$stu$r
 
 
 
-foreach ($i in (1..3)){
+foreach ($i in (1..$DISKAMOUNT)){
     write-host  (get-content $srt$stu$i)
 }
 
